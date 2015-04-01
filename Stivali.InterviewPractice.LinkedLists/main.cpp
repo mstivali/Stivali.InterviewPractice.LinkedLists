4ca1c053f2fec8c137f049f99a13cddbae402e32
mstivali <mstivali@mail.usf.edu>
2015-03-31 22:31:22 -0400

    Initial Commit

---
+++ Stivali.InterviewPractice.LinkedLists/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Stivali.InterviewPractice.LinkedLists
+//
+//  Created by Matt on 3/31/15.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
